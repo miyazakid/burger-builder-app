c43be6ee51909ecd626d6cdfcb845d96f6a83dda
miyzakid <mfintan@gmail.com>
2019-08-11 22:37:00 -0400

    Section 8 Lecture 179

---
+++ src/components/Navigation/SideDrawer/MenuToggle/MenuToggle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './MenuToggle.module.css';
+
+const menuToggle = (props) => (
+    <div className={classes.MenuToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default menuToggle;
