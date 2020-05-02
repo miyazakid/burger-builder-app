8f5fe1e59b67b19c2e403b8a9e8caf804c928f67
Fintan Maher <34825464+miyazakid@users.noreply.github.com>
2020-05-02 17:42:32 -0400

    Section 18 Lecture 328
    Storing the token

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
   state = {
@@ -35,7 +37,8 @@ class Auth extends Component {
         valid: false,
         touched: false
       }
-    }
+    },
+    isSignup: true
   }
 
   checkValidity(value, rules) {
@@ -69,6 +72,18 @@ class Auth extends Component {
       this.setState({controls: updatedControls});
   }
 
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {isSignup: !prevState.isSignup};
+    });
+  }
+
   render () {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -92,13 +107,20 @@ class Auth extends Component {
     ));
     return (
         <div className={classes.AuthData}>
-          <form>
+          <form onSubmit={this.submitHandler}>
             {form}
             <Button btnType="Success">SUBMIT</Button>
           </form>
+          <Button btnType="Danger" clicked={this.switchAuthModeHandler}>SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
         </div>
     );
   }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+      onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
